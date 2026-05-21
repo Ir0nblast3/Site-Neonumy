7938fcaf80fcfea449d1247c3674aab1709b7100
Ir0nblast3 <tpafonso1@gmail.com>
2026-05-21 17:49:03 +0100

    .
    .

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-et40#b-b59r)680bue*yv68h45)5ve7l5sy#^+x+_sqae@tebw"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
