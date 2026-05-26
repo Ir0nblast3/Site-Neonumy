ed3c5d203018966676e33d3258489926057a895c
Ir0nblast3 <tpafonso1@gmail.com>
2026-05-26 17:58:08 +0100

    footer start

--- mysite/base/models.py
+++ mysite/base/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 
 
 @register_setting
@@ -16,4 +16,60 @@ class NavigationSettings(BaseGenericSetting):
 
     panels = [
         FieldPanel("logo"),
+    ]
+
+
+@register_setting
+class FooterSettings(BaseGenericSetting):
+    
+    facebook_url = models.URLField(verbose_name="Facebook URL", blank=True)
+    facebook_icon= models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+
+    instagram_url = models.URLField(verbose_name="Instagram URL", blank=True)
+    instagram_icon = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    linkedin_url = models.URLField(verbose_name="LinkedIn URL", blank=True)
+    linkedin_icon = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    whatsapp_url = models.URLField(verbose_name="WhatsApp URL", blank=True)
+    whatsapp_icon = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel("facebook_url"),
+                FieldPanel("facebook_icon"),
+                FieldPanel("instagram_url"),
+                FieldPanel("instagram_icon"),
+                FieldPanel("linkedin_url"),
+                FieldPanel("linkedin_icon"),
+                FieldPanel("whatsapp_url"),
+                FieldPanel("whatsapp_icon"),
+            ],
+            "Social settings",
+        )
     ]
