2f2b69c5570fb4029f0fcd2068ff46322b073412
Ir0nblast3 <tpafonso1@gmail.com>
2026-06-22 17:47:47 +0100

    Adding gallery to team and tech sections

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -1,9 +1,55 @@
 from django.db import models
-from wagtail.admin.panels import FieldPanel
-from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel, InlinePanel
+from wagtail.models import Page, Orderable
 from wagtail.fields import StreamField
 from wagtail.images.blocks import ImageChooserBlock
+from modelcluster.fields import ParentalKey
 
+class TechnologyItem(Orderable):
+    page = ParentalKey(
+        'HomePage',
+        on_delete=models.CASCADE,
+        related_name='technologies'
+    )
+
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    text = models.CharField(max_length=255, blank=True)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('text'),
+    ]
+
+class TeamMembers(Orderable):
+    page = ParentalKey(
+        'HomePage',
+        on_delete=models.CASCADE,
+        related_name='teammembers'
+    )
+
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    name = models.CharField(max_length=255, blank=True)
+    role = models.CharField(max_length=255, blank=True)
+
+    panels = [
+        FieldPanel('image'),
+        FieldPanel('name'),
+        FieldPanel('role'),
+    ]
 
 class PrivacyPolicyPage(Page):
 
@@ -60,86 +106,6 @@ class HomePage(Page):
     )
 
     #OUR TECNOLOGIES
-    
-    django_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-
-    django_text = models.CharField(max_length=255, blank=True)
-
-    docker_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-
-    docker_text = models.CharField(max_length=255, blank=True)
-
-    flutter_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-
-    flutter_text = models.CharField(max_length=255, blank=True)
-
-    graphql_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-
-    graphql_text = models.CharField(max_length=255, blank=True)
-
-    hashi_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-
-    hashi_text = models.CharField(max_length=255, blank=True)
-
-    mongo_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-
-    mongo_text = models.CharField(max_length=255, blank=True)
-
-    postgre_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-
-    postgre_text = models.CharField(max_length=255, blank=True)
-
-    react_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+'
-    )
-
-    react_text = models.CharField(max_length=255, blank=True)
 
     #ABOUT US
 
@@ -251,56 +217,21 @@ class HomePage(Page):
 
 
     content_panels = Page.content_panels + [
-    FieldPanel("hero_title"),
-    FieldPanel("hero_subtitle"),
-    FieldPanel("hero_image"),
-    FieldPanel("newrai_image"),
+    MultiFieldPanel([
+        FieldPanel("hero_title"),
+        FieldPanel("hero_subtitle"),
+        FieldPanel("hero_image"),
+        FieldPanel("newrai_image"),
 
-    FieldPanel("django_image"),
-    FieldPanel("django_text"),
-    FieldPanel("docker_image"),
-    FieldPanel("docker_text"),
-    FieldPanel("flutter_image"),
-    FieldPanel("flutter_text"),
-    FieldPanel("graphql_image"),
-    FieldPanel("graphql_text"),
-    FieldPanel("hashi_image"),
-    FieldPanel("hashi_text"),
-    FieldPanel("mongo_image"),
-    FieldPanel("mongo_text"),
-    FieldPanel("postgre_image"),
-    FieldPanel("postgre_text"),
-    FieldPanel("react_image"),
-    FieldPanel("react_text"),
+    ], heading="Hero"),
+
+    InlinePanel('technologies', label='Technologies'),
 
     FieldPanel("About_Us_Text_1"),
     FieldPanel("Aboutus_image"),
     FieldPanel("About_Us_Text_2"),
     FieldPanel("aboutus_gallery"),
 
-    FieldPanel("Image_1"),
-    FieldPanel("Name_Text_1"),
-    FieldPanel("Role_Text_1"),
-    FieldPanel("Image_2"),
-    FieldPanel("Name_Text_2"),
-    FieldPanel("Role_Text_2"),
-    FieldPanel("Image_3"),
-    FieldPanel("Name_Text_3"),
-    FieldPanel("Role_Text_3"),
-    FieldPanel("Image_4"),
-    FieldPanel("Name_Text_4"),
-    FieldPanel("Role_Text_4"),
-    FieldPanel("Image_5"),
-    FieldPanel("Name_Text_5"),
-    FieldPanel("Role_Text_5"),
-    FieldPanel("Image_6"),
-    FieldPanel("Name_Text_6"),
-    FieldPanel("Role_Text_6"),
-    FieldPanel("Image_7"),
-    FieldPanel("Name_Text_7"),
-    FieldPanel("Role_Text_7"),
-    FieldPanel("Image_8"),
-    FieldPanel("Name_Text_8"),
-    FieldPanel("Role_Text_8"),
+    InlinePanel('teammembers', label='Members'),
 ]
 
